a51db603c0061e1f8911e1f6ec2c2a3642d1f47d
Andrew Plummer <plummer574@gmail.com>
2016-02-06 17:36:58 +0000

    Add .python_emacs.sh script

---
+++ .python_emacs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ~/
+sudo rm -rf ropemacs_things
+mkdir ropemacs_things
+cd ropemacs_things
+
+git clone https://github.com/python-rope/ropemacs.git
+cd ropemacs
+sudo python setup.py install
+cd ..
+
+git clone https://github.com/python-rope/ropemode.git
+cd ropemode
+sudo python setup.py install
+
+git clone https://github.com/pinard/Pymacs
+cd Pymacs
+make check
+sudo make install
+cd ..
